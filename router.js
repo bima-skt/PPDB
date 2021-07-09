01dc681b11dcc6c9301346a6997cde95c00f3571
bima-skt <75791467+bima-skt@users.noreply.github.com>
2021-07-09 19:15:41 +0700

    project
    project PPDB

---
+++ router.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const Route = express.Router();
+
+Route.get('/', (req, res) => res.render('index'));
+Route.get('/tentang', (req, res) => res.render('tentang'));
+Route.get('/artikel', (req, res) => res.render('artikel'));
+Route.get('/artikel1', (req, res) => res.render('artikel_1'));
+Route.get('/artikel2', (req, res) => res.render('artikel_2'));
+Route.get('/artikel3', (req, res) => res.render('artikel_3'));
+
+export default Route;
